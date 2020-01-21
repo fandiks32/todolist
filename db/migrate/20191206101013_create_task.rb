23a667c2d05cdeb52dfc97ec29b1d45edceeef66
irfan <irfandiofficial32@gmail.com>
2020-01-21 09:34:12 +0900

    Add devise auth

--- db/migrate/20191206101013_create_task.rb
+++ db/migrate/20191206101013_create_task.rb
@@ -1,6 +1,7 @@
 class CreateTask < ActiveRecord::Migration[5.1]
   def change
     create_table :tasks do |t|
+      t.integer   :user_id
       t.string    :description
       t.integer   :priority
       t.string    :status
