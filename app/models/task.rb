26847e4ef1001342ea2ba02894fc42a805923d85
irfan <irfandiofficial32@gmail.com>
2020-01-23 11:16:55 +0900

    add deadline order

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,7 @@
 class Task < ActiveRecord::Base
   belongs_to :user
   
-  scope :default_order, -> { order(status: :desc, priority: :asc, description: :asc) }
+  scope :default_order, -> { order(status: :desc, deadline: :asc, priority: :asc, description: :asc) }
   scope :by_user, ->(user) { where(user: user) }
   validates :priority, numericality: { only_integer: true, allow_nil: false, message: 'Error value on Priority' }
   validates :description, presence: true
